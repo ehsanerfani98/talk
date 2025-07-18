cf00cdf646f4f6adf5b857d0212a439d5eca0d62
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-07-18 14:31:58 +0330

    clear fix

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,22 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "89.251.8.198",
+            port: 5173,
+        },
+    },
+});
+
 // import { defineConfig } from 'vite';
 // import laravel from 'laravel-vite-plugin';
 
@@ -11,29 +30,8 @@
 //     server: {
 //         host: '0.0.0.0',
 //         hmr: {
-//             host: 'localhost',
+//             host: 'talkd.test',
 //             port: 5173,
 //         },
 //     },
 // });
-
-
-
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'talkd.test',
-            port: 5173,
-        },
-    },
-});
